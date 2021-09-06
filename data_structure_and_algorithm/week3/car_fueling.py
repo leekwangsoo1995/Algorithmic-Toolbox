3e709e2047409a61f41a41881c8ccba9d6a86f51
leekwangsoo1995 <47682473+leekwangsoo1995@users.noreply.github.com>
2021-09-07 08:56:26 +0900

    car_fueling_problem

---
+++ data_structure_and_algorithm/week3/car_fueling.py
@@ -0,0 +1,37 @@
+# Uses python3
+import sys
+
+
+def compute_min_refills(distance, tank, stops):
+    full_tank = tank
+    count = 0
+    prev_stop = 0
+    if (distance - tank) < 0:
+        return 0
+    for stop in stops:
+        stop = int(stop)
+        dis = stop - prev_stop
+        if dis > tank and dis > full_tank:
+            return -1
+        else:
+            if dis > tank:
+                tank = full_tank
+                count += 1
+            tank = tank - dis
+            prev_stop = stop
+
+    distance = distance - stop
+    if (distance - tank) < 0:
+        return count
+    else:
+        if distance > full_tank:
+            return -1
+        else:
+            return count + 1
+
+d = int(input())
+m = int(input())
+n = int(input())
+stops = input().split()
+
+print(compute_min_refills(d, m, stops))
